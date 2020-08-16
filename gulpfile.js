4e7f9646d0875f15488edf5a4ec01bcad22dd5db
Luna Wen <lunawenresonate@gmail.com>
2020-08-16 23:32:56 +1000

    update lock file

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
 "use strict";
 
-let gulp = require('gulp');
-let jshint = require('gulp-jshint');
+let gulp = require("gulp");
+let jshint = require("gulp-jshint");
 
-gulp.task('default',['jshint','test','serve']);
+// gulp
+//     .task("jshint", gulp.series(() => {
+//         return gulp
+//             // .series("clean")
+//             .src("./*.js")
+//             .pipe(jshint())
+//             .pipe(jshint.reporter("default"));
+//     }))
+//     .task("test", gulp.series(() => {
+//         require("./test.js");
+// 	}))
+gulp
+    .task("serve", gulp.series(() => {
+        require("./main.js");
+    }));
 
-gulp.task('jshint',()=>{
-	return gulp.src('./*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'));
-})
-.task('test',()=>{
-	require('./test.js');
-})
-.task('serve',()=>{
-	require('./main.js');
-});
+// gulp.task("default", gulp.series('clean', gulp.parallel("jshint", "test", "serve")));
+gulp.task("default", gulp.series("serve"));
