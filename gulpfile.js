16cdccd1ac97414c910f80800b62ce74a1bf9a2a
Luna Wen <lunawen33@gmail.com>
2020-08-17 07:35:44 +1000

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,38 @@
-"use strict";
+// "use strict";
 
-let gulp = require("gulp");
+// let gulp = require("gulp");
 let jshint = require("gulp-jshint");
 
-gulp
-    .task("jshint", gulp.series(() => {
-        return gulp
-            .src("./*.js")
-            .pipe(jshint())
-            .pipe(jshint.reporter("default"));
-    }));
-gulp
-    .task("test", gulp.series(() => {
-        require("./test.js");
-    }));
-gulp
-    .task("serve", gulp.series(() => {
-        require("./main.js");
-    }));
+// gulp
+//     .task("jshint", gulp.series(() => {
+        
+//     }));
+// gulp
+//     .task("test", gulp.series(() => {
+//         require("./test.js");
+//     }));
+// gulp
+//     .task("serve", gulp.series(() => {
+//         require("./main.js");
+//     }));
 
-gulp.task("default", gulp.series("jshint", "serve", "test"));
+const { series, src } = require('gulp');
+function js(cb){
+    src("./*.js")
+    .pipe(jshint())
+    .pipe(jshint.reporter("default"));
+    cb();
+}
+
+function serve(cb){
+    require("./main.js");
+    cb();
+}
+
+function test(cb){
+    require("./test.js");
+    cb();
+}
+
+exports.default = series(js, serve, test);
+exports.test = series(js, serve, test);
