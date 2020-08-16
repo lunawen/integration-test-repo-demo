3620f93ded18f0b5297406245cf947c415fa8377
Luna Wen <lunawenresonate@gmail.com>
2020-08-16 23:35:33 +1000

    update lock file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let gulp = require("gulp");
-let jshint = require("gulp-jshint");
+// let jshint = require("gulp-jshint");
 
 // gulp
 //     .task("jshint", gulp.series(() => {
@@ -13,11 +13,11 @@ let jshint = require("gulp-jshint");
 //     }))
 //     .task("test", gulp.series(() => {
 //         require("./test.js");
-// 	}))
+//     }))
 gulp
     .task("serve", gulp.series(() => {
         require("./main.js");
     }));
 
-// gulp.task("default", gulp.series('clean', gulp.parallel("jshint", "test", "serve")));
+// gulp.task("default", gulp.parallel("jshint", "test", "serve"));
 gulp.task("default", gulp.series("serve"));
